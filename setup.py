d0d4645e4d1526167b64ffbc23ab8bfe9c039861
Robert Buchholz <rbu@goodpoint.de>
2018-05-30 12:14:53 +0200

    Remove unused files

--- setup.py
+++ setup.py
@@ -10,13 +10,20 @@ setup(name='Products.ZSQLiteDA',
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
-        "Programming Language :: Python",
-        "Framework :: Zope2",
+        "Development Status :: 6 - Mature",
+        "Environment :: Web Environment",
+        "Framework :: Zope :: 4",
         "License :: OSI Approved :: Zope Public License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         ],
       keywords='SQLite',
-      author='Hajime Nakagami',
-      author_email='nakagami@gmail.com',
+      author='HOFFMANN+LIEBENBERG in association with SNTL Publishing, Hajime Nakagami',
+      author_email='zms@sntl-publishing.com',
       url='https://github.com/nakagami/Products.ZSQLiteDA',
       license='ZPL',
       packages=find_packages(exclude=['ez_setup']),
@@ -24,11 +31,6 @@ setup(name='Products.ZSQLiteDA',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
-          'ThreadLock',
           'Products.ZSQLMethods>=3.0.2',
       ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
       )
